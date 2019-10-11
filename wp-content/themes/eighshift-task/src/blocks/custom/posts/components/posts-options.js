fa7d484d8e72eec1e184c07400f4c5d4700a228b
Gabriel Glogoški <gabriel.glogoski@infinum.hr>
2019-10-11 15:08:03 +0200

    Add post number option

--- wp-content/themes/eighshift-task/src/blocks/custom/posts/components/posts-options.js
+++ wp-content/themes/eighshift-task/src/blocks/custom/posts/components/posts-options.js
@@ -1,10 +1,26 @@
 import { __ } from '@wordpress/i18n';
-import { PanelBody } from '@wordpress/components';
+import { PanelBody, RangeControl } from '@wordpress/components';
 
 export const PostsOptions = (props) => {
+  const {
+    attributes: {
+      postNumber,
+    },
+    actions: {
+      onChangePostNumber,
+    },
+  } = props;
+
   return (
     <PanelBody title={__('Posts Settings', 'eightshift-blocks')}>
-
+      <RangeControl
+        label="Number of posts to load"
+        value={postNumber}
+        onChange={onChangePostNumber}
+        initialPosition={postNumber}
+        min={1}
+        max={20}
+      />
     </PanelBody>
   );
 };

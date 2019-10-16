921899522109bd2eccff9b8f5f607336b312747c
Gabriel Glogoški <gabriel.glogoski@infinum.hr>
2019-10-16 13:22:03 +0200

    Update load more button style

--- wp-content/themes/eighshift-task/src/blocks/custom/posts/parts/load-more.php
+++ wp-content/themes/eighshift-task/src/blocks/custom/posts/parts/load-more.php
@@ -12,7 +12,7 @@ if ( ! isset( $pagination_query ) ) {
 
 if ( $pagination_query instanceof \WP_Query && $pagination_query->max_num_pages > 1 ) {
   ?>
-  <button class="btn btn__color--primary btn__size--small js-load-more" data-query="<?php echo esc_attr( wp_json_encode( $pagination_query->query ) ); ?>" data-page="<?php echo esc_attr( get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1 ); ?>" data-max-pages="<?php echo esc_attr( $pagination_query->max_num_pages ); ?>" data-parent-class="<?php echo esc_attr( $parent_block_class ); ?>">
+  <button class="btn btn__color--primary btn__size--small btn-load-more js-load-more" data-query="<?php echo esc_attr( wp_json_encode( $pagination_query->query ) ); ?>" data-page="<?php echo esc_attr( get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1 ); ?>" data-max-pages="<?php echo esc_attr( $pagination_query->max_num_pages ); ?>" data-parent-class="<?php echo esc_attr( $parent_block_class ); ?>">
     <?php esc_html_e( 'Load More', 'eightshift-task' ); ?>
   </button>
   <?php

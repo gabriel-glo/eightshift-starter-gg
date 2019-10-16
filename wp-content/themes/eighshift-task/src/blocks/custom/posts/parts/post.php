87635db538189849cf515806b1b1b376964e5880
Gabriel Glogoški <gabriel.glogoski@infinum.hr>
2019-10-16 09:35:52 +0200

    Initial posts load

---
+++ wp-content/themes/eighshift-task/src/blocks/custom/posts/parts/post.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Template for the single post article.
+ *
+ * @since 1.0.0
+ * @package Eighshift_Task\Blocks.
+ */
+
+namespace Eighshift_Task\Blocks;
+
+$parent_class = $attributes['parentClass'] ?? '';
+$url          = $attributes['url'] ?? '';
+$title        = $attributes['title'] ?? '';
+$excerpt      = $attributes['excerpt'] ?? '';
+$categories   = $attributes['categories'] ?? '';
+$date         = $attributes['date'] ?? '';
+
+$featured_image = $attributes['featuredImageThumb'] ?? '';
+
+$post_classes = "
+  {$parent_class}__post
+";
+
+$img_classes = "
+  {$parent_class}__post-image
+";
+
+$date_classes = "
+  {$parent_class}__post-date
+";
+
+$heading_classes = "
+  heading__size--small
+  {$parent_class}__post-title
+";
+
+$excerpt_classes = "
+  paragraph__size--default
+  {$parent_class}__post-excerpt
+";
+
+$categories_classes = "
+  {$parent_class}__post-category-list
+";
+
+$category_classes = "
+  {$parent_class}__post-category
+";
+
+?>
+<a href="<?php echo esc_url( $url ); ?>" class="<?php echo esc_attr( $post_classes ); ?>">
+  <?php
+  if ( ! empty( $featured_image ) ) {
+    ?>
+    <img src="<?php echo esc_url( $featured_image ); ?>" alt="" class="<?php echo esc_attr( $img_classes ); ?>">
+    <?php
+  }
+  ?>
+
+  <?php
+  if ( ! empty( $date ) ) {
+    ?>
+    <span class="<?php echo esc_attr( $date_classes ); ?>">
+      <?php echo esc_html( $date ); ?>
+    </span>
+    <?php
+  }
+  ?>
+
+  <?php
+  if ( ! empty( $title ) ) {
+    ?>
+    <h4 class="<?php echo esc_attr( $heading_classes ); ?>">
+      <?php echo esc_html( $title ); ?>
+    </h4>
+    <?php
+  }
+  ?>
+
+  <?php
+  if ( ! empty( $excerpt ) ) {
+    ?>
+    <p class="<?php echo esc_attr( $excerpt_classes ); ?>">
+      <?php echo esc_html( $excerpt ); ?>
+    </p>
+    <?php
+  }
+  ?>
+
+  <?php
+  if ( ! empty( $categories ) ) {
+    ?>
+    <div class="<?php echo esc_attr( $categories_classes ); ?>">
+      <?php
+      foreach ( $categories as $category ) {
+        ?>
+        <span class="<?php echo esc_attr( $category_classes ); ?>">
+          <?php echo esc_html( $category->name ); ?>
+        </span>
+        <?php
+      }
+      ?>
+    </div>
+    <?php
+  }
+  ?>
+</a>
--- wp-content/themes/eighshift-task/src/blocks/custom/posts/posts.php
+++ wp-content/themes/eighshift-task/src/blocks/custom/posts/posts.php
@@ -3,15 +3,25 @@
  * Template for the Example Block view.
  *
  * @since 1.0.0
- * @package Eightshift_Boilerplate\Blocks.
+ * @package Eighshift_Task\Blocks.
  */
 
-namespace Eightshift_Boilerplate\Blocks;
+namespace Eighshift_Task\Blocks;
 
-$block_class = $attributes['blockClass'] ?? '';
+$block_class    = $attributes['blockClass'] ?? '';
+$block_js_class = $attributes['blockJsClass'] ?? '';
+$post_number    = $attributes['postNumber'] ?? 4;
+
+$wrapper_classes = "
+  {$block_class}__wrap
+";
+
+$listing_classes = "
+  {$block_class}__listing
+  {$block_js_class}
+";
 
 ?>
-
 <div class="<?php echo esc_attr( $block_class ); ?>">
   <?php
   $this->render_block_view(
@@ -25,5 +35,56 @@ $block_class = $attributes['blockClass'] ?? '';
       'styleSize' => $attributes['headingStyleSize'] ?? '',
     ]
   );
+
+  $posts_query = new \WP_Query(
+    [
+      'post_type' => 'post',
+      'posts_per_page' => $post_number,
+    ]
+  );
+
+  $post_list = $posts_query->posts;
   ?>
+  <div class="<?php echo esc_attr( $wrapper_classes ); ?>">
+    <?php
+    if ( ! empty( $post_list ) ) {
+      ?>
+      <div class="<?php echo esc_attr( $listing_classes ); ?>" data-post-number="<?php echo esc_attr( $post_number ); ?>">
+        <?php
+        foreach ( $post_list as $single_post ) {
+          $single_post_date = new \DateTime( $single_post->post_date );
+          $attachment_id    = get_post_thumbnail_id( $single_post->ID );
+
+          $this->render_block_view(
+            '/custom/posts/parts/post.php',
+            [
+              'parentClass'        => $block_class,
+              'url'                => get_the_permalink( $single_post->ID ),
+              'title'              => $single_post->post_title,
+              'excerpt'            => $single_post->post_excerpt,
+              'featuredImageThumb' => wp_get_attachment_image_src( $attachment_id, 'thumbnail' )[0],
+              'categories'         => get_the_terms( $single_post->ID, 'category' ),
+              'date'               => $single_post_date->format( 'd.m.Y @ G:i' ),
+            ]
+          );
+        }
+        ?>
+      </div>
+      <?php
+    }
+
+    $pagination_query   = $posts_query;
+    $parent_block_class = $block_class;
+
+    wp_reset_postdata();
+
+    $load_more_button = locate_template( 'src/blocks/custom/posts/parts/load-more.php' );
+
+    if ( ! empty( $load_more_button ) ) {
+      include $load_more_button;
+    }
+
+    wp_nonce_field( 'js-ajax-post-load-nonce', 'ajax-post-load-nonce' );
+    ?>
+  </div>
 </div>
